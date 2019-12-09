f5ec0330bbef49684c136f473500e73652e18c27
Benedikt Werner <1benediktwerner@gmail.com>
2019-12-09 18:21:45 +0100

    Fix error reporting location

--- compiler/src/parser.rs
+++ compiler/src/parser.rs
@@ -7,6 +7,14 @@ pub enum Line {
     Stmt(crate::ast::Stmt),
 }
 
+pub struct SourcePos(usize, usize);
+
+impl std::fmt::Display for SourcePos {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}:{}", self.0, self.1)
+    }
+}
+
 fn unop(op: UnOp, a: Expr) -> Expr {
     Expr::UnOp(op, Box::new(a))
 }
@@ -15,19 +23,36 @@ fn binop(a: Expr, op: BinOp, b: Expr) -> Expr {
     Expr::BinOp(Box::new(a), op, Box::new(b))
 }
 
-pub fn parse<'a>(
-    input: &'a str,
-) -> Result<(Program, IdentEnv), impl std::error::Error + std::fmt::Display + 'a> {
+fn map_location(input: &str, index: usize) -> SourcePos {
+    let mut line = 1;
+    let mut column = 1;
+
+    for (i, c) in input.char_indices() {
+        if i == index {
+            break;
+        }
+        if c == '\n' {
+            line += 1;
+            column = 1;
+        } else {
+            column += 1;
+        }
+    }
+
+    SourcePos(line, column)
+}
+
+pub fn parse<'a>(input: &'a str) -> Result<(Program, IdentEnv), impl std::fmt::Display + 'a> {
     let mut env = IdentEnv::new();
     match grammar::ProgramParser::new().parse(&mut env, input) {
         Ok(program) => Ok((program, env)),
-        Err(error) => Err(error),
+        Err(error) => Err(error.map_location(|p| map_location(input, p))),
     }
 }
 
 pub fn strip_comments(s: String) -> String {
     let mut result = Vec::new();
-    let regex = regex::Regex::new(r"//.*").unwrap();
+    let regex = regex::Regex::new(r"\s*//.*").unwrap();
     for line in s.lines() {
         result.push(regex.replace(line, ""));
     }
